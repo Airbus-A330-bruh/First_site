bc64f7831fdf0d47284d378452f6e50556400cdb
A330-300 <qwertyuiop12341323@proton.me>
2026-04-21 09:37:08 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,54 @@
+<script>
+// "unique" id? no
+// sorry for the cringe name lol
+const randomNum = Math.floor(Math.random() * 1000);
+const peer = new Peer('test-id-' + Math.floor(Math.random() * 1000), { // random id generation
+  config: {
+    'iceServers': [
+      { urls: 'stun:stun.l.google.com:19302' }, 
+      { urls: 'stun:stun1.l.google.com:19302' }                        // stun servers to fix device communication i think
+    ]
+  }
+});
+
+// share ur id to be even more vulnerable yay
+peer.on('open', (id) => {
+  document.getElementById('my-id').innerText = id;
+});
+
+
+peer.on('connection', (incomingConn) => {
+  conn = incomingConn;
+  setupChat();
+});
+
+
+function connectToPeer() {
+  const remoteId = document.getElementById("peerIdInput").value;
+  conn = peer.connect(remoteId);
+  setupChat();
+}
+
+
+function setupChat() {
+  conn.on('data', (data) => {
+   
+    document.getElementById("output").innerHTML += "<div>Peer sent: " + data + "</div>";
+  });
+}
+
+
+function inject() {
+  const userInput = document.getElementById("xssInput").value;
+  
+  // show it on screen
+  document.getElementById("output").innerHTML += "<div>You: " + userInput + "</div>";
+
+  // If connected, send input to peer
+  if (conn && conn.open) {
+    conn.send(userInput);
+  }
+
+
+}
+</script>
